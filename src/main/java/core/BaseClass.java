4e6843a7bc58d3be1a4e03d82fdc33e8c5f9b7bb
adityahkshirsagar <105305236+adityahkshirsagar@users.noreply.github.com>
2024-04-23 14:59:40 +0530

    Update BaseClass.java
    Removed thread local from base class

--- src/main/java/core/BaseClass.java
+++ src/main/java/core/BaseClass.java
@@ -18,7 +18,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-import core.ThreadLocalClass;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
